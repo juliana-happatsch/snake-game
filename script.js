bb844caf48ef4763b6b79e0f21f61be4c2e343ed
juliana-happatsch <jhappatsch@gmail.com>
2021-12-04 14:25:16 -0300

    Create food

--- script.js
+++ script.js
@@ -6,8 +6,18 @@ const box = 32;
 // snake
 const snake = [];
 snake[0] = { x: 8 * box, y: 8 * box };
-let direction = 'right';
 
+// moviment
+let direction = 'right';
+document.addEventListener('keydown', update);
+
+// food
+const food = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box,
+};
+
+// functions
 function createBG() {
   context.fillStyle = 'lightgreen';
   context.fillRect(0, 0, 16 * box, 16 * box);
@@ -20,9 +30,27 @@ function createSnake() {
   }
 }
 
+function update(e) {
+  if (e.keyCode === 37 && direction !== 'right') direction = 'left';
+  if (e.keyCode === 39 && direction !== 'left') direction = 'right';
+  if (e.keyCode === 38 && direction !== 'down') direction = 'up';
+  if (e.keyCode === 40 && direction !== 'up') direction = 'down';
+}
+
+function drawFood() {
+  context.fillStyle = 'red';
+  context.fillRect(food.x, food.y, box, box);
+}
+
 function startGame() {
+  if (snake[0].x > 15 * box && direction === 'right') snake[0].x = 0;
+  if (snake[0].x < 0 && direction === 'left') snake[0].x = 16 * box;
+  if (snake[0].y > 15 * box && direction === 'down') snake[0].y = 0;
+  if (snake[0].y < 0 && direction === 'up') snake[0].y = 16 * box;
+
   createBG();
   createSnake();
+  drawFood();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
