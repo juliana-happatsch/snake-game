a2bd266f8f141b92b353f8b6d94d5475b93640d5
juliana-happatsch <jhappatsch@gmail.com>
2021-12-04 14:37:24 -0300

    Create feature to make snake grow

--- script.js
+++ script.js
@@ -42,16 +42,12 @@ function drawFood() {
   context.fillRect(food.x, food.y, box, box);
 }
 
-function startGame() {
+function changeDirection() {
   if (snake[0].x > 15 * box && direction === 'right') snake[0].x = 0;
   if (snake[0].x < 0 && direction === 'left') snake[0].x = 16 * box;
   if (snake[0].y > 15 * box && direction === 'down') snake[0].y = 0;
   if (snake[0].y < 0 && direction === 'up') snake[0].y = 16 * box;
-
-  createBG();
-  createSnake();
-  drawFood();
-
+  
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
 
@@ -60,7 +56,12 @@ function startGame() {
   if (direction === 'up') snakeY -= box;
   if (direction === 'down') snakeY += box;
 
-  snake.pop();
+  if (snakeX !== food.x || snakeY !== food.y) {
+    snake.pop();
+  } else {
+    food.x = Math.floor(Math.random() * 15 + 1) * box;
+    food.y =  Math.floor(Math.random() * 15 + 1) * box;
+  }
 
   let newHead = {
     x: snakeX,
@@ -68,6 +69,13 @@ function startGame() {
   };
 
   snake.unshift(newHead);
+}
+
+function startGame() {
+  createBG();
+  createSnake();
+  changeDirection();
+  drawFood();
 }
 
 const game = setInterval(startGame, 100);
